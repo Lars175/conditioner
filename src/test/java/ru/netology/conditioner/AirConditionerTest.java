8c59ab1ab90bc52d6857d812bf45e4ffc9af39e9
Larisa Karelina <karelina.rss@gmail.com>
2020-06-04 17:54:21 +0300

    editing tests

--- src/test/java/ru/netology/conditioner/AirConditionerTest.java
+++ src/test/java/ru/netology/conditioner/AirConditionerTest.java
@@ -61,4 +61,24 @@ class AirConditionerTest {
         int actual = conditioner.getCurrentTemperature();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void setOn() {
+        AirConditioner conditioner = new AirConditioner();
+        conditioner.setOn(true);
+        conditioner.setCurrentTemperature(10);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(11, conditioner.getCurrentTemperature());
+        assertEquals(true, conditioner.isOn());
+    }
+
+    @Test
+    public void setOff() {
+        AirConditioner conditioner = new AirConditioner();
+        conditioner.setOn(false);
+        conditioner.setCurrentTemperature(9);
+        assertEquals(9, conditioner.getCurrentTemperature());
+
+        assertEquals(false, conditioner.isOn());
+    }
 }
